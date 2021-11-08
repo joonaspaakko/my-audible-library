3ae59db1b25521c4c413d826e632219b5e41316b
Joonas Pääkkö <paakkoj@gmail.com>
2021-11-08 19:47:11 +0200

    v.0.2.7

--- README.md
+++
@@ -1,2 +0,0 @@
-# ALE-page
-My Audible library generated with [audible library extractor](https://github.com/joonaspaakko/audible-library-extractor)
---
+++ data/split-book-data/B091ZDMF3H.1636393443312.js
@@ -0,0 +1,1 @@
+window.bookSummaryJSON = "Brynn Morris lands an administrative job at KillCo., but is swept up in a company-wide competition that puts her at odds with her morals."; 
